b7b47bf4a0e883a3c1de1f0195a749a469431f8d
ShaziaAthar <75155946+ShaziaAthar@users.noreply.github.com>
2020-12-02 17:07:49 -0800

    made few changes

--- shazia-athar-final-project/scripts/final.js
+++ shazia-athar-final-project/scripts/final.js
@@ -31,12 +31,12 @@ const btnclose=document.getElementById('win');
 let counter=0;
 
   buttonrolldice.addEventListener('click',function(){
-    counter++
+   counter++
 
-  const randomedice1=Math.floor(Math.random()*6)+1;
-  const randomedice2=Math.floor(Math.random()*6)+1;
-  const randomedice3=Math.floor(Math.random()*6)+1;
-  const randomedice4=Math.floor(Math.random()*6)+1;
+   const randomedice1=Math.floor(Math.random()*6)+1;
+   const randomedice2=Math.floor(Math.random()*6)+1;
+   const randomedice3=Math.floor(Math.random()*6)+1;
+   const randomedice4=Math.floor(Math.random()*6)+1;
 
   const ran1=randomedice1+randomedice2;
   const ran2=randomedice3+randomedice4;
@@ -47,52 +47,60 @@ let counter=0;
   dice4.src=(`images/dice-${randomedice4}.jpg`);
 
 //conditions to print scores
-  if(randomedice1===1){
-    yourscore.innerHTML=0
-    yourtotalscore.innerHTML=0;
-    }
-  else{ 
-     yourscore.innerHTML=ran1}
-  if(randomedice2===1){
-       yourscore.innerHTML=0
-       yourtotalscore.innerHTML=0;
-    } 
-  else{
-        oppscore.innerHTML=ran1}
-  if(randomedice3===1){
-        oppscore.innerHTML=0
-        opptotalscore.innerHTML=0;
-    } 
-  else{
-        oppscore.innerHTML=ran2}
-  if(randomedice4===1){
-      oppscore.innerHTML=0
-      opptotalscore.innerHTML=0;
-    }
-    else{
-        oppscore.innerHTML=ran2} 
-  if(randomedice1===randomedice2 ){
-     yourtotalscore.innerHTML=ran1*2}
-   else{
-         yourtotalscore.innerHTML= ran1 }     
-  if(randomedice3===randomedice4){
-     opptotalscore.innerHTML =ran2*2}
-  else{
-     opptotalscore.innerHTML=ran2};
+if(randomedice1===1 || randomedice2===1){ 
+  yourscore.innerHTML= 0;
+  yourtotalscore.innerHTML= 0; }
+        else{ 
+         yourscore.innerHTML=ran1;
+         yourtotalscore.innerHTML=ran1;
+         //if(counter===1){yourtotalscore.innrHTML=ran1}
+         //if(counter===2){yourtotalscore.innerHTML=ran1+ran1}
+         //if(counter===3){yourtotalscore.innerHTML=ran1+ran1+ran1}
+        }
+if(randomedice1===randomedice2 ){
+        yourtotalscore.innerHTML=ran1*2;}
+        else{
+         yourscore.innerHTML=ran1;
+        //yourtotalscore.innerHTML=ran1+ran1
+        //if(counter===1){yourtotalscore.innrHTML=ran1+ran1}
+        //if(counter===2){yourtotalscore.innerHTML=ran1+ran1}
+        // if(counter===3){yourtotalscore.innerHTML=ran1+ran1+ran1}
+        }
+     
+if(randomedice3===1 || randomedice4===1){
+  oppscore.innerHTML= 0;
+  opptotalscore.innerHTML= 0; } 
+        else{
+            oppscore.innerHTML=ran2;
+            opptotalscore.innerHTML=ran2;
+           // if(counter===1){opptotalscore.innrHTML=ran2}
+           // if(counter===2){opptotalscore.innerHTML=ran2+ran2}
+            //if(counter===3){opptotalscore.innerHTML=ran2+ran2+ran2}
+        }
+if(randomedice3===randomedice4){
+      opptotalscore.innerHTML =ran2*2;}
+       else{
+            oppscore.innerHTML=ran2;
+            //opptotalscore.innerHTML=ran2
+            //if(counter===1){yourtotalscore.innrHTML=ran2}
+           //if(counter===2){yourtotalscore.innerHTML=ran2+ran2}
+           // if(counter===3){yourtotalscore.innerHTML=ran2+ran2+ran2}
+           }
 
 //conditions for result output
   if(counter===3){
     popup.style.opacity='1';
   }
   if(ran1>ran2){
-     win.innerHTML="You Win"
+     win.innerHTML="You Win";
   }
-  else{
-      win.innerHTML="Opponent Wins"}
-   if(yourtotalscore===opptotalscore){
-       win.innerHTML="Game Draw"
+  if(ran1<ran2){
+      win.innerHTML="Opponent Wins";
+    }
+   if(ran1===ran2){
+       win.innerHTML="Game Draw";
    } 
-})
+});
 //event for start new game
   buttonnewgame.addEventListener('click',function(){
      counter=0; 
@@ -105,7 +113,7 @@ let counter=0;
   dice3.src=" ";
   dice4.src=" ";
   popup.style.opacity=0
-  })
+  });
   
  
     
